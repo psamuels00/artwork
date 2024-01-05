5edb48e22eeae4b53adcac78bd10a51fda763f7b
Perrin Samuels <perrin@aristotle>
2024-01-05 15:00:41 -0800

    move projects data to global scope

---
+++ src/_data/projects.js
@@ -0,0 +1,95 @@
+module.exports = {
+  {
+    name: 'Backyard Hills',
+    images: [
+      'Backyard-09-finished-cropped.jpg',
+      'Backyard-01-orig-drawing.jpg',
+      'Backyard-04-both-drawings.jpg',
+      'Backyard-05-ref-photo.jpg',
+      'Backyard-06-in-progress-1.jpg',
+      'Backyard-07-in-progress-2.jpg',
+      'Backyard-08-finished.jpg',
+    ],
+  },
+  {
+    name: 'Emma',
+    images: [
+      'EmmaSittingRelaxedAttentive-12-final-framed.jpg',
+      'EmmaSittingRelaxedAttentive-13-final-zoom-head.jpg',
+      'EmmaSittingRelaxedAttentive-14-final-zoom-face.jpg',
+      'EmmaSittingRelaxedAttentive-02-photo.jpg',
+      'EmmaSittingRelaxedAttentive-03-demo.jpg',
+      'EmmaSittingRelaxedAttentive-04-photo-and-drawing.jpg',
+      'EmmaSittingRelaxedAttentive-05-first-drawing.jpg',
+      'EmmaSittingRelaxedAttentive-06-second-drawing.jpg',
+      'EmmaSittingRelaxedAttentive-07-ground.jpg',
+      'EmmaSittingRelaxedAttentive-08-floor.jpg',
+      'EmmaSittingRelaxedAttentive-09-counter-bg.jpg',
+      'EmmaSittingRelaxedAttentive-10-dark-body.jpg',
+      'EmmaSittingRelaxedAttentive-11-final.jpg',
+    ],
+  },
+  {
+    name: 'Beach Boardwalk',
+    images: [
+      'BeachBoardwalkSketch-02-bench.jpg',
+      'BeachBoardwalkSketch-01-full.jpg',
+      'BeachBoardwalkSketch-03-barge.jpg',
+      'BeachBoardwalkSketch-04-lifeguard.jpg',
+      'BeachBoardwalkSketch-05-kite.jpg',
+      'BeachBoardwalkSketch-06-surfers.jpg',
+    ],
+  },
+  {
+    name: 'Desert Flower',
+    images: [
+      'ColorWheelDesertFlower-01-final.jpg',
+    ],
+  },
+  {
+    name: 'The Demo',
+    images: [
+      '02-PaintingFull.jpg',
+      '01-OriginalPhoto.jpg',
+      '03-DummyAndDummy.jpg',
+      '04-Surfer.jpg',
+      '05-Dummy.jpg',
+      '06-DummyHead.jpg',
+      '07-Dummy2.jpg',
+      '08-Dummy2Head.jpg',
+      '09-Dummy2Face.jpg',
+      '10-Dummy2Eyes.jpg',
+      '11-Dummy2Mouth.jpg',
+      '12-Dummy2LeftHand.jpg',
+      '13-Dummy2RightHand.jpg',
+      '14-Fence.jpg',
+      '15-DummyLeftHand.jpg',
+      '16-DummyRightHand.jpg',
+      '17-RightHands.jpg',
+      '18-Signature.jpg',
+    ],
+  },
+  {
+    name: 'Dolphin',
+    images: [
+      '02-TheDolphin.jpg',
+      '01-TheArtistAndTheDolphin.jpg',
+      '03-TheDolphinCropped.jpg',
+    ],
+  },
+  {
+    name: 'The Onion',
+    images: [
+      '01-RedOnion.jpg',
+      '02-RedOnionCloseUp.jpg',
+      '03-TheArtistAndRedOnion.jpg',
+      '04-RedOnionFullWall.jpg',
+    ],
+  },
+  {
+    name: 'Starry Night',
+    images: [
+      '01-StarryStarryNight.jpg',
+    ],
+  },
+}
