c74ce5ad3bb365ad68b9b1618ccf5f91d4212ab8
Perrin Samuels <perrin@aristotle>
2024-01-07 20:10:40 -0800

    add vertical space to code

--- .eleventy.js
+++ .eleventy.js
@@ -3,12 +3,14 @@ module.exports = config => {
   config.addPassthroughCopy({
     'global.out.css': 'global.css',
   });
+
   config.addFilter('rmFileExt', value => {
     return value.replace(/\..+$/, '');
   });
   config.addFilter('cleanupHack', value => {
     return value.replace(/^(.+-)?\d\d-/, '');
   });
+
   return {
     dir: {
       input: 'src',
