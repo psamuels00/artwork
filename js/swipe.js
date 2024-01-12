5e3b612208018b22747faa22b638ba6e8a363689
psamuels00 <psamuels00@users.noreply.github.com>
2024-01-12 05:19:17 +0000

    deploy: dc96d270ca3b4949a0087f7c7c0e4d4721d1cf7d

--- js/swipe.js
+++ js/swipe.js
@@ -1,61 +1,18 @@
-function detectSwipe(elem_id, func) {
-  let points = { sX: 0, sY: 0, eX: 0, eY: 0 };
-
-  const min_x = 30;
-  const max_x = 30;
-  const min_y = 50;
-  const max_y = 60;
-
-  elem = document.getElementById(elem_id);
-
-  elem.addEventListener('touchstart', e => {
-    const t = e.touches[0];
-    points.sX = t.screenX;
-    points.sY = t.screenY;
-  });
-
-  elem.addEventListener('touchmove', e => {
-    const t = e.touches[0];
-    points.eX = t.screenX;
-    points.eY = t.screenY;
-
-    const horzX = (points.eX - min_x > points.sX) || (points.eX + min_x < points.sX);
-    const horzY = (points.eY < points.sY + max_y) && (points.sY > points.eY - max_y) && (points.eX > 0);
-    if (horzX && horzY) {
-        e.preventDefault();
-    }
-  });
-
-  elem.addEventListener('touchend', e => {
-    const horzX = (points.eX - min_x > points.sX) || (points.eX + min_x < points.sX);
-    const horzY = (points.eY < points.sY + max_y) && (points.sY > points.eY - max_y) && (points.eX > 0);
-    const vertX = (points.eY - min_y > points.sY) || (points.eY + min_y < points.sY)
-    const vertY = (points.eX < points.sX + max_x) && (points.sX > points.eX - max_x) && (points.eY > 0)
-
-    let direction = '';
-    if (horzX && horzY) {
-      direction = (points.eX > points.sX) ? 'r' : 'l';
-    } else if (vertX && vertY) {
-      direction = (points.eY > points.sY) ? 'd' : 'up';
-    }
-
-    if (['l', 'r'].includes(direction)) {
-      e.preventDefault();
-      func(elem_id, e, direction);
-    }
-
-    points = { sX: 0, sY: 0, eX: 0, eY: 0 };
-  });
-}
-
 function swipeLeftRight(elem_id, leftFunc, rightFunc) {
-  function dispatchSwipe(elem_id, event, direction) {
-    if (direction === 'r') {
-      leftFunc(event);
-    } else if (direction === 'l') {
-      rightFunc(event);
-    }
-  }
+  elem = document.getElementById(elem_id);
+  const manager = new Hammer.Manager(elem);
 
-  detectSwipe(elem_id, dispatchSwipe);
+  const swipe = new Hammer.Swipe({
+    direction: Hammer.DIRECTION_HORIZONTAL
+  });
+  manager.add(swipe);
+
+  manager.on('swipe', function(e) {
+    let direction = e.offsetDirection;
+    if (direction === 2) {
+      rightFunc();
+    } else if (direction === 4) {
+      leftFunc();
+    }
+  });
 }
