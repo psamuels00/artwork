29475f4fcfb2df1480da5f689d82e5c813541a5a
Perrin Samuels <perrin@descartes>
2024-01-14 13:50:11 -0800

    improve documentation of mobile viewport hack

--- tailwind.config.js
+++ tailwind.config.js
@@ -18,14 +18,20 @@ module.exports = {
         },
       },
       maxHeight: {
-        // 142px and 205px are the combined height of the navigation headers
-        // 10px is an arbitrary margin
-        // 210px and 105px allow room for the Title and first line of Description to be seen
+        // For top image on Portfolio project pages
+        //   o  142px and 205px are the combined height of the navigation headers
+        //   o  10px is an arbitrary margin
+        //   o  210px and 105px allow room for the Title and first line of Description to be seen
         fit_in_viewport: 'calc(100vh - 142px - 10px - 210px)',
         fit_in_viewport_lg: 'calc(100vh - 205px - 10px - 105px)',
 
-        // and similarly for single image pages, except no margin needed bc images have padding
-        // ...although  there is a mystery 110px additional adjustment needed for iPhone!?
+        // For Doodles single-image pages
+        //   o  144px is the combined height of the navigation headers
+        //   o  no extra margin is needed because the images all have padding
+        //   o  110px is a hack to compensate for browser navigation controls covering part of
+        //      the viewport on iPhone and other mobile devices.  This is a subject of hot debate
+        //      and hack proposals abound.  As I couldn't get any of the more effective-looking
+        //      hacks to work, this dreadful one remains.
         fit_in_viewport_single: 'calc(100vh - 144px - 110px)',
         fit_in_viewport_single_sm: 'calc(100vh - 144px)',
       },
