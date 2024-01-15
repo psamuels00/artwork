b3b33ddbdfee946404d702c252623a2a32f31877
Perrin Samuels <perrin@descartes>
2024-01-15 14:59:26 -0800

    improve parsing title from image file name

--- .eleventy.js
+++ .eleventy.js
@@ -24,7 +24,19 @@ module.exports = config => {
     return value.replace(/^(.+-)?\d\d-/, '');
   });
   config.addFilter('sepWords', value => {
-    return value.replace(/([a-z])([A-Z])/g, "$1 $2");
+    return value
+      .replace(/-/g, ' ')
+      .replace(/([a-z])([A-Z])/g, '$1 $2')
+      .replace(/([A-Z])([A-Z])/g, '$1 $2')
+      .replace(/(\d+)([a-zA-Z_])/g, '$1 $2')
+      .replace(/([a-zA-Z_])(\d+)/g, '$1 $2')
+  });
+
+  // image title
+  config.addFilter('capWords', value => {
+    return value.replace(/\b\w/g, function(match) {
+        return match.toUpperCase();
+    });
   });
 
   // image orientation
@@ -35,7 +47,7 @@ module.exports = config => {
   });
 
   // copyright year
-  config.addFilter("yearSince", value => {
+  config.addFilter('yearSince', value => {
     year = new Date().getFullYear();
     return (year === value) ? year : ` ${value}-${year}`;
   });
