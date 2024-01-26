9b5b5a1a050a153355bb71ab831dbb066ac75128
Perrin Samuels <perrin@descartes>
2024-01-26 14:40:17 -0800

    fix image links on about/resume pages

--- src/_eleventy/figure.js
+++ src/_eleventy/figure.js
@@ -4,12 +4,10 @@ const outdent = require('outdent');
 
 const shortcode = async (src, alt, title) => {
   imageClass = 'swipeable';
-  const image = await imageShortcode(src, alt, imageClass)
+  const image = await imageShortcode(src, alt, imageClass, {linkable: true})
 
   const figure = `<figure class="mt-8 sm:mt-0 mx-16 sm:mx-8 md:mx-20">
-      <a href="${src}">
-        ${image}
-      </a>
+      ${image}
       <figcaption class="mt-2 mb-16 sm:mb-8">${title}</figcaption>
   </figure>`;
 
