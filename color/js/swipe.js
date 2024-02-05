a75c5e84b9c50e739c58e92051df20e845e8d895
psamuels00 <psamuels00@users.noreply.github.com>
2024-02-05 08:24:46 +0000

    deploy: 1f52b3c8cbd3e27233ff6ed2c23e68154682aaa2

---
+++ color/js/swipe.js
@@ -0,0 +1,24 @@
+function swipeLeftRight(leftFunc, rightFunc) {
+  elements = document.getElementsByClassName('swipeable');
+  for (let elem of elements) {
+      swipeLeftRightElem(elem, leftFunc, rightFunc);
+  }
+}
+
+function swipeLeftRightElem(elem, leftFunc, rightFunc) {
+  const manager = new Hammer.Manager(elem);
+
+  const swipe = new Hammer.Swipe({
+    direction: Hammer.DIRECTION_HORIZONTAL
+  });
+  manager.add(swipe);
+
+  manager.on('swipe', function(e) {
+    let direction = e.offsetDirection;
+    if (direction === 2) {
+      rightFunc();
+    } else if (direction === 4) {
+      leftFunc();
+    }
+  });
+}
